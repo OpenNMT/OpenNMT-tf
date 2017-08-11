2b6a16579080ddb488dc3ab9737d100ede892470
Guillaume Klein <guillaume.klein@systrangroup.com>
2017-08-11 15:46:36 +0200

    Raise an error if n_best is greater than the beam width

--- opennmt/models/sequence_to_sequence.py
+++ opennmt/models/sequence_to_sequence.py
@@ -173,6 +173,9 @@ class SequenceToSequence(Model):
     n_best = params and params.get("n_best")
     n_best = n_best or 1
 
+    if n_best > len(prediction["tokens"]):
+      raise ValueError("n_best cannot be greater than beam_width")
+
     all_preds = []
     for i in range(n_best):
       tokens = prediction["tokens"][i][:prediction["length"][i] - 1] # Ignore </s>.
