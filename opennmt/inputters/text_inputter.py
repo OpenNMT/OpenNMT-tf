2fdd82136b38a08bc62ed3c60a21f24f107706e5
Guillaume Klein <guillaume.klein@systrangroup.com>
2017-09-22 17:42:46 +0200

    Fix embedding metadata path

--- opennmt/inputters/text_inputter.py
+++ opennmt/inputters/text_inputter.py
@@ -59,14 +59,14 @@ def visualize_embeddings(log_dir, embedding_var, vocabulary_file, num_oov_bucket
   exists = False
   for meta in config.embeddings:
     if meta.tensor_name == embedding_var.name:
-      meta.metadata_path = basename
+      meta.metadata_path = destination
       exists = True
       break
 
   if not exists:
     embedding = config.embeddings.add()
     embedding.tensor_name = embedding_var.name
-    embedding.metadata_path = basename
+    embedding.metadata_path = destination
 
   summary_writer = tf.summary.FileWriter(log_dir)
 
