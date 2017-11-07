d961388e8df29af67bf60a6269469eff8b84d6a7
Guillaume Klein <guillaumekln@users.noreply.github.com>
2017-11-07 11:04:44 +0100

    Update the serving example to rely on remote batching (#15)

--- examples/serving/nmt_client.py
+++ examples/serving/nmt_client.py
@@ -12,76 +12,46 @@ from tensorflow_serving.apis import predict_pb2
 from tensorflow_serving.apis import prediction_service_pb2
 
 
-def print_batch(batch_tokens):
-  """Convenience function to display a batch of tokens."""
-  for tokens in batch_tokens:
-    print("\t{}".format(" ".join(tokens)))
-
-def pad_batch(batch_tokens):
-  """Pads a batch of tokens.
-
-  Args:
-    batch_tokens: A list of list of strings.
-
-  Returns:
-    batch_tokens: A list of right-padded list of strings.
-    lengths: A list of int containing the length of each sequence.
-  """
-  max_length = 0
-  for tokens in batch_tokens:
-    max_length = max(max_length, len(tokens))
-  lengths = []
-  for tokens in batch_tokens:
-    length = len(tokens)
-    tokens += [""] * (max_length - length)
-    lengths.append(length)
-  return batch_tokens, lengths
-
 def parse_translation_result(result):
-  """Parses the server result.
+  """Parses a translation result.
 
   Args:
     result: A `PredictResponse` proto.
 
   Returns:
-    A list of list of strings (a.k.a a batch of tokens).
+    A list of tokens.
   """
-  lengths = tf.make_ndarray(result.outputs["length"])
-  batch_predictions = tf.make_ndarray(result.outputs["tokens"])
-  best = []
-  for hypotheses, length in zip(batch_predictions, lengths):
-    # Only consider the first hypothesis (the best one).
-    best_hypotheses = hypotheses[0]
-    best_length = length[0]
-    best.append(best_hypotheses[0:best_length - 1]) # Ignore </s>.
-  return best
+  lengths = tf.make_ndarray(result.outputs["length"])[0]
+  hypotheses = tf.make_ndarray(result.outputs["tokens"])[0]
 
-def translate_batch(stub, model_name, batch_tokens, timeout=5.0):
-  """Translates a batch.
+  # Only consider the first hypothesis (the best one).
+  best_hypothesis = hypotheses[0]
+  best_length = lengths[0]
+
+  return best_hypothesis[0:best_length - 1] # Ignore </s>
+
+def translate(stub, model_name, tokens, timeout=5.0):
+  """Translates a sequence of tokens.
 
   Args:
     stub: The prediction service stub.
     model_name: The model to request.
-    batch_tokens: The batch to translate as a list of list of strings.
+    tokens: A list of tokens.
     timeout: Timeout after this many seconds.
 
   Returns:
-    The translation as a list of list of strings.
+    A future.
   """
-  batch_tokens, lengths = pad_batch(batch_tokens)
-  batch_size = len(batch_tokens)
-  max_length = len(batch_tokens[0])
+  length = len(tokens)
 
   request = predict_pb2.PredictRequest()
   request.model_spec.name = model_name
-
   request.inputs["tokens"].CopyFrom(
-      tf.make_tensor_proto(batch_tokens, shape=(batch_size, max_length)))
+      tf.make_tensor_proto([tokens], shape=(1, length)))
   request.inputs["length"].CopyFrom(
-      tf.make_tensor_proto(lengths, shape=(batch_size,)))
+      tf.make_tensor_proto([length], shape=(1,)))
 
-  result_future = stub.Predict.future(request, timeout)
-  return parse_translation_result(result_future.result())
+  return stub.Predict.future(request, timeout)
 
 def main():
   parser = argparse.ArgumentParser(description="Translation client example")
@@ -91,30 +61,27 @@ def main():
                       help="model server host")
   parser.add_argument("--port", type=int, default=9000,
                       help="model server port")
-  parser.add_argument("--timeout", type=float, default=5.0,
+  parser.add_argument("--timeout", type=float, default=10.0,
                       help="request timeout")
   args = parser.parse_args()
 
   channel = implementations.insecure_channel(args.host, args.port)
   stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
 
-  # Prepare a simple batch request with 3 sentences.
   batch_tokens = [
       ["Hello", "world", "!"],
       ["My", "name", "is", "John", "."],
       ["I", "live", "on", "the", "West", "coast", "."]]
 
-  print("Input:")
-  print_batch(batch_tokens)
+  futures = []
+  for tokens in batch_tokens:
+    future = translate(stub, args.model_name, tokens, timeout=args.timeout)
+    futures.append(future)
 
-  output_batch_tokens = translate_batch(
-      stub,
-      args.model_name,
-      batch_tokens,
-      timeout=args.timeout)
+  for tokens, future in zip(batch_tokens, futures):
+    result = parse_translation_result(future.result())
+    print("{} ||| {}".format(" ".join(tokens), " ".join(result)))
 
-  print("Output:")
-  print_batch(output_batch_tokens)
 
 if __name__ == "__main__":
   main()
