568d4310265a2d4aaea8d160790f3a9776a3f85b
Guillaume Klein <guillaume.klein@systrangroup.com>
2018-02-09 14:34:46 +0100

    Create Runner class to manage training, inference, and export

--- bin/main.py
+++ bin/main.py
@@ -3,17 +3,12 @@
 import argparse
 import json
 import os
-import sys
-import random
-import pickle
 import six
 
-import numpy as np
 import tensorflow as tf
 
-from opennmt.utils import hooks
-from opennmt.utils.evaluator import external_evaluation_fn
-from opennmt.config import load_model_module, load_config
+from opennmt.runner import Runner
+from opennmt.config import load_model, load_config
 
 
 def _prefix_paths(prefix, paths):
@@ -37,180 +32,6 @@ def _prefix_paths(prefix, paths):
       return new_path
     else:
       return path
-
-def load_model(model_dir, model_file=None):
-  """Loads the model.
-
-  The model object is pickled in `model_dir` to make the model configuration
-  optional for future runs.
-
-  Args:
-    model_dir: The model directory.
-    model_file: An optional model configuration.
-
-  Returns:
-    A `opennmt.models.Model` object.
-  """
-  serial_model_file = os.path.join(model_dir, "model_description.pkl")
-
-  if model_file:
-    if tf.train.latest_checkpoint(model_dir) is not None:
-      tf.logging.warn(
-          "You provided a model configuration but a checkpoint already exists. "
-          "The model configuration must define the same model as the one used for "
-          "the initial training. However, you can change non structural values like "
-          "dropout.")
-
-    model_config = load_model_module(model_file)
-    model = model_config.model()
-
-    with open(serial_model_file, "wb") as serial_model:
-      pickle.dump(model, serial_model)
-  elif not os.path.isfile(serial_model_file):
-    raise RuntimeError("A model configuration is required.")
-  else:
-    tf.logging.info("Loading serialized model description from %s", serial_model_file)
-    with open(serial_model_file, "rb") as serial_model:
-      model = pickle.load(serial_model)
-
-  return model
-
-def train(estimator, model, config, num_devices=1):
-  """Runs training.
-
-  Args:
-    estimator: A `tf.estimator.Estimator`.
-    model: A `opennmt.models.Model`.
-    config: The configuration.
-    num_devices: The number of devices used for training.
-  """
-  if "eval" not in config:
-    config["eval"] = {}
-
-  train_hooks = [
-      hooks.LogParametersCountHook(),
-      hooks.CountersHook(
-          every_n_steps=estimator.config.save_summary_steps,
-          output_dir=estimator.model_dir)]
-
-  eval_hooks = []
-  if (config["eval"].get("save_eval_predictions", False)
-      or config["eval"].get("external_evaluators") is not None):
-    save_path = os.path.join(estimator.model_dir, "eval")
-    if not os.path.isdir(save_path):
-      os.makedirs(save_path)
-    eval_hooks.append(hooks.SaveEvaluationPredictionHook(
-        model,
-        os.path.join(save_path, "predictions.txt"),
-        post_evaluation_fn=external_evaluation_fn(
-            config["eval"].get("external_evaluators"),
-            config["data"]["eval_labels_file"],
-            output_dir=estimator.model_dir)))
-
-  default_sample_buffer_size = 1000000
-  if "sample_buffer_size" not in config["train"]:
-    tf.logging.warn("You did not set sample_buffer_size. By default, the "
-                    "training dataset is shuffled by chunk of %d examples. "
-                    "If your dataset is larger than this value and eval_delay "
-                    "is shorter than the training time of one epoch, a section "
-                    "of the dataset will be discarded. Consider setting "
-                    "sample_buffer_size to the size of your dataset."
-                    % default_sample_buffer_size)
-
-  train_batch_size = config["train"]["batch_size"]
-  train_batch_type = config["train"].get("batch_type", "examples")
-  train_spec = tf.estimator.TrainSpec(
-      input_fn=model.input_fn(
-          tf.estimator.ModeKeys.TRAIN,
-          train_batch_size,
-          config["data"],
-          config["data"]["train_features_file"],
-          labels_file=config["data"]["train_labels_file"],
-          batch_type=train_batch_type,
-          batch_multiplier=num_devices,
-          bucket_width=config["train"].get("bucket_width", 5),
-          num_threads=config["train"].get("num_threads"),
-          sample_buffer_size=config["train"].get(
-              "sample_buffer_size", default_sample_buffer_size),
-          maximum_features_length=config["train"].get("maximum_features_length"),
-          maximum_labels_length=config["train"].get("maximum_labels_length")),
-      max_steps=config["train"].get("train_steps"),
-      hooks=train_hooks)
-
-  eval_batch_size = config["eval"].get(
-      "batch_size", train_batch_size if train_batch_type == "examples" else 30)
-  eval_spec = tf.estimator.EvalSpec(
-      input_fn=model.input_fn(
-          tf.estimator.ModeKeys.EVAL,
-          eval_batch_size,
-          config["data"],
-          config["data"]["eval_features_file"],
-          num_threads=config["eval"].get("num_threads"),
-          labels_file=config["data"]["eval_labels_file"]),
-      steps=None,
-      hooks=eval_hooks,
-      exporters=tf.estimator.LatestExporter("latest", model.serving_input_fn(config["data"])),
-      throttle_secs=config["eval"].get("eval_delay", 18000))
-
-  tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
-
-def infer(features_file,
-          estimator,
-          model,
-          config,
-          checkpoint_path=None,
-          predictions_file=None):
-  """Runs inference and prints predictions on the standard output.
-
-  Args:
-    features_file: The file to infer from.
-    estimator: A `tf.estimator.Estimator`.
-    model: A `opennmt.models.Model`.
-    config: The configuration.
-    checkpoint_path: Path of a specific checkpoint to predict. If `None`, the
-      latest is used.
-    predictions_file: If set, predictions are saved in this file.
-  """
-  if "infer" not in config:
-    config["infer"] = {}
-
-  batch_size = config["infer"].get("batch_size", 1)
-  input_fn = model.input_fn(
-      tf.estimator.ModeKeys.PREDICT,
-      batch_size,
-      config["data"],
-      features_file,
-      num_threads=config["infer"].get("num_threads"))
-
-  if predictions_file:
-    stream = open(predictions_file, "w")
-  else:
-    stream = sys.stdout
-
-  for prediction in estimator.predict(input_fn=input_fn, checkpoint_path=checkpoint_path):
-    model.print_prediction(prediction, params=config["infer"], stream=stream)
-
-  if predictions_file:
-    stream.close()
-
-def export(estimator, model, config, checkpoint_path=None):
-  """Exports a model.
-
-  Args:
-    estimator: A `tf.estimator.Estimator`.
-    model: A `opennmt.models.Model`.
-    config: The configuration.
-    checkpoint_path: The checkpoint path to export. If `None`, the latest is used.
-  """
-  export_dir = os.path.join(estimator.model_dir, "export")
-  if not os.path.isdir(export_dir):
-    os.makedirs(export_dir)
-
-  estimator.export_savedmodel(
-      os.path.join(export_dir, "manual"),
-      model.serving_input_fn(config["data"]),
-      checkpoint_path=checkpoint_path)
-
 
 def main():
   parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -274,69 +95,36 @@ def main():
 
   # Load and merge run configurations.
   config = load_config(args.config)
-
   if args.run_dir:
     config["model_dir"] = os.path.join(args.run_dir, config["model_dir"])
+  if args.data_dir:
+    config["data"] = _prefix_paths(args.data_dir, config["data"])
+
   if not os.path.isdir(config["model_dir"]):
     tf.logging.info("Creating model directory %s", config["model_dir"])
     os.makedirs(config["model_dir"])
 
-  session_config = tf.ConfigProto(
-      allow_soft_placement=True,
-      log_device_placement=False,
-      gpu_options=tf.GPUOptions(
-          allow_growth=args.gpu_allow_growth))
-
-  run_config = tf.estimator.RunConfig(
-      model_dir=config["model_dir"],
-      session_config=session_config,
-      tf_random_seed=args.seed)
-
-  np.random.seed(args.seed)
-  random.seed(args.seed)
-
-  if "train" in config:
-    if "save_summary_steps" in config["train"]:
-      run_config = run_config.replace(
-          save_summary_steps=config["train"]["save_summary_steps"],
-          log_step_count_steps=config["train"]["save_summary_steps"])
-    if "save_checkpoints_steps" in config["train"]:
-      run_config = run_config.replace(
-          save_checkpoints_secs=None,
-          save_checkpoints_steps=config["train"]["save_checkpoints_steps"])
-    if "keep_checkpoint_max" in config["train"]:
-      run_config = run_config.replace(
-          keep_checkpoint_max=config["train"]["keep_checkpoint_max"])
-
   model = load_model(config["model_dir"], model_file=args.model)
-
-  estimator = tf.estimator.Estimator(
-      model.model_fn(num_devices=args.num_gpus),
-      config=run_config,
-      params=config["params"])
-
-  checkpoint_path = args.checkpoint_path
-  if checkpoint_path is not None and os.path.isdir(checkpoint_path):
-    checkpoint_path = tf.train.latest_checkpoint(checkpoint_path)
+  runner = Runner(
+      model,
+      config,
+      seed=args.seed,
+      num_devices=args.num_gpus,
+      gpu_allow_growth=args.gpu_allow_growth)
 
   if args.run == "train":
-    if args.data_dir:
-      config["data"] = _prefix_paths(args.data_dir, config["data"])
-    train(estimator, model, config, num_devices=args.num_gpus)
+    runner.train()
   elif args.run == "infer":
     if not args.features_file:
       parser.error("--features_file is required for inference.")
     elif len(args.features_file) == 1:
       args.features_file = args.features_file[0]
-    infer(
+    runner.infer(
         args.features_file,
-        estimator,
-        model,
-        config,
-        checkpoint_path=checkpoint_path,
-        predictions_file=args.predictions_file)
+        predictions_file=args.predictions_file,
+        checkpoint_path=args.checkpoint_path)
   elif args.run == "export":
-    export(estimator, model, config, checkpoint_path=checkpoint_path)
+    runner.export(checkpoint_path=args.checkpoint_path)
 
 
 if __name__ == "__main__":
---
+++ opennmt/runner.py
@@ -0,0 +1,191 @@
+"""Main library entrypoint."""
+
+import os
+import sys
+import random
+
+import numpy as np
+import tensorflow as tf
+
+from opennmt.utils import hooks
+from opennmt.utils.evaluator import external_evaluation_fn
+
+
+class Runner(object):
+  """Class for managing training, inference, and export. It is mostly a
+  wrapper around ``tf.estimator.Estimator``.
+  """
+
+  def __init__(self,
+               model,
+               config,
+               seed=None,
+               num_devices=1,
+               gpu_allow_growth=False):
+    """Initializes the runner parameters.
+
+    Args:
+      model: A :class:`opennmt.models.model.Model` instance to run.
+      config: The run configuration.
+      seed: The random seed to set.
+      num_devices: The number of devices (GPUs) to use for training.
+      gpu_allow_growth: Allow GPU memory to grow dynamically.
+    """
+    self._model = model
+    self._config = config
+    self._num_devices = num_devices
+
+    session_config = tf.ConfigProto(
+        allow_soft_placement=True,
+        log_device_placement=False,
+        gpu_options=tf.GPUOptions(
+            allow_growth=gpu_allow_growth))
+    run_config = tf.estimator.RunConfig(
+        model_dir=self._config["model_dir"],
+        session_config=session_config,
+        tf_random_seed=seed)
+
+    np.random.seed(seed)
+    random.seed(seed)
+
+    if "train" in self._config:
+      if "save_summary_steps" in self._config["train"]:
+        run_config = run_config.replace(
+            save_summary_steps=self._config["train"]["save_summary_steps"],
+            log_step_count_steps=self._config["train"]["save_summary_steps"])
+      if "save_checkpoints_steps" in self._config["train"]:
+        run_config = run_config.replace(
+            save_checkpoints_secs=None,
+            save_checkpoints_steps=self._config["train"]["save_checkpoints_steps"])
+      if "keep_checkpoint_max" in self._config["train"]:
+        run_config = run_config.replace(
+            keep_checkpoint_max=self._config["train"]["keep_checkpoint_max"])
+
+    self._estimator = tf.estimator.Estimator(
+        self._model.model_fn(num_devices=self._num_devices),
+        config=run_config,
+        params=self._config["params"])
+
+  def train(self):
+    """Runs training."""
+    if "eval" not in self._config:
+      self._config["eval"] = {}
+
+    train_hooks = [
+        hooks.LogParametersCountHook(),
+        hooks.CountersHook(
+            every_n_steps=self._estimator.config.save_summary_steps,
+            output_dir=self._estimator.model_dir)]
+
+    eval_hooks = []
+    if (self._config["eval"].get("save_eval_predictions", False)
+        or self._config["eval"].get("external_evaluators") is not None):
+      save_path = os.path.join(self._estimator.model_dir, "eval")
+      if not os.path.isdir(save_path):
+        os.makedirs(save_path)
+      eval_hooks.append(hooks.SaveEvaluationPredictionHook(
+          self._model,
+          os.path.join(save_path, "predictions.txt"),
+          post_evaluation_fn=external_evaluation_fn(
+              self._config["eval"].get("external_evaluators"),
+              self._config["data"]["eval_labels_file"],
+              output_dir=self._estimator.model_dir)))
+
+    default_sample_buffer_size = 1000000
+    if "sample_buffer_size" not in self._config["train"]:
+      tf.logging.warn("You did not set sample_buffer_size. By default, the "
+                      "training dataset is shuffled by chunk of %d examples. "
+                      "If your dataset is larger than this value and eval_delay "
+                      "is shorter than the training time of one epoch, a section "
+                      "of the dataset will be discarded. Consider setting "
+                      "sample_buffer_size to the size of your dataset."
+                      % default_sample_buffer_size)
+
+    train_batch_size = self._config["train"]["batch_size"]
+    train_batch_type = self._config["train"].get("batch_type", "examples")
+    train_spec = tf.estimator.TrainSpec(
+        input_fn=self._model.input_fn(
+            tf.estimator.ModeKeys.TRAIN,
+            train_batch_size,
+            self._config["data"],
+            self._config["data"]["train_features_file"],
+            labels_file=self._config["data"]["train_labels_file"],
+            batch_type=train_batch_type,
+            batch_multiplier=self._num_devices,
+            bucket_width=self._config["train"].get("bucket_width", 5),
+            num_threads=self._config["train"].get("num_threads"),
+            sample_buffer_size=self._config["train"].get(
+                "sample_buffer_size", default_sample_buffer_size),
+            maximum_features_length=self._config["train"].get("maximum_features_length"),
+            maximum_labels_length=self._config["train"].get("maximum_labels_length")),
+        max_steps=self._config["train"].get("train_steps"),
+        hooks=train_hooks)
+
+    eval_batch_size = self._config["eval"].get(
+        "batch_size", train_batch_size if train_batch_type == "examples" else 30)
+    eval_spec = tf.estimator.EvalSpec(
+        input_fn=self._model.input_fn(
+            tf.estimator.ModeKeys.EVAL,
+            eval_batch_size,
+            self._config["data"],
+            self._config["data"]["eval_features_file"],
+            num_threads=self._config["eval"].get("num_threads"),
+            labels_file=self._config["data"]["eval_labels_file"]),
+        steps=None,
+        hooks=eval_hooks,
+        exporters=tf.estimator.LatestExporter(
+            "latest", self._model.serving_input_fn(self._config["data"])),
+        throttle_secs=self._config["eval"].get("eval_delay", 18000))
+
+    tf.estimator.train_and_evaluate(self._estimator, train_spec, eval_spec)
+
+  def infer(self, features_file, predictions_file=None, checkpoint_path=None):
+    """Runs inference.
+
+    Args:
+      features_file: The file(s) to infer from.
+      predictions_file: If set, predictions are saved in this file.
+      checkpoint_path: Path of a specific checkpoint to predict. If ``None``,
+        the latest is used.
+    """
+    if "infer" not in self._config:
+      self._config["infer"] = {}
+    if checkpoint_path is not None and os.path.isdir(checkpoint_path):
+      checkpoint_path = tf.train.latest_checkpoint(checkpoint_path)
+
+    batch_size = self._config["infer"].get("batch_size", 1)
+    input_fn = self._model.input_fn(
+        tf.estimator.ModeKeys.PREDICT,
+        batch_size,
+        self._config["data"],
+        features_file,
+        num_threads=self._config["infer"].get("num_threads"))
+
+    if predictions_file:
+      stream = open(predictions_file, "w")
+    else:
+      stream = sys.stdout
+
+    for prediction in self._estimator.predict(input_fn=input_fn, checkpoint_path=checkpoint_path):
+      self._model.print_prediction(prediction, params=self._config["infer"], stream=stream)
+
+    if predictions_file:
+      stream.close()
+
+  def export(self, checkpoint_path=None):
+    """Exports a model.
+
+    Args:
+      checkpoint_path: The checkpoint path to export. If ``None``, the latest is used.
+    """
+    if checkpoint_path is not None and os.path.isdir(checkpoint_path):
+      checkpoint_path = tf.train.latest_checkpoint(checkpoint_path)
+
+    export_dir = os.path.join(self._estimator.model_dir, "export")
+    if not os.path.isdir(export_dir):
+      os.makedirs(export_dir)
+
+    self._estimator.export_savedmodel(
+        os.path.join(export_dir, "manual"),
+        self._model.serving_input_fn(self._config["data"]),
+        checkpoint_path=checkpoint_path)
