e510b0e5db9521645d576dff0397d516e4585bdd
Guillaume Klein <guillaume.klein@systrangroup.com>
2018-03-05 15:49:37 +0100

    Merge configurations of the EnDe training

--- scripts/wmt/run_wmt_ende.sh
+++ scripts/wmt/run_wmt_ende.sh
@@ -2,5 +2,7 @@
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
-PYTHONPATH=../.. python -m bin.main train --model ../../config/models/transformer.py \
-  --config config/wmt_ende.yml config/adam_with_noam_decay.yml --num_gpus 4
+PYTHONPATH=../.. python -m bin.main train \
+  --model ../../config/models/transformer.py \
+  --config config/wmt_ende.yml \
+  --num_gpus 4
