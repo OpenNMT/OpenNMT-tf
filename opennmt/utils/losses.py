8a4590e6b0c3a2445ab763a60dcb430abc7ccc45
Guillaume Klein <guillaume.klein@systrangroup.com>
2018-03-07 16:55:26 +0100

    Use tf.nn.softmax_cross_entropy_with_logits_v2

--- opennmt/utils/losses.py
+++ opennmt/utils/losses.py
@@ -16,7 +16,7 @@ def _smooth_one_hot_labels(logits, labels, label_smoothing):
 def _softmax_cross_entropy(logits, labels, label_smoothing, mode):
   if mode == tf.estimator.ModeKeys.TRAIN and label_smoothing > 0.0:
     smoothed_labels = _smooth_one_hot_labels(logits, labels, label_smoothing)
-    return tf.nn.softmax_cross_entropy_with_logits(
+    return tf.nn.softmax_cross_entropy_with_logits_v2(
         logits=logits, labels=smoothed_labels)
   else:
     return tf.nn.sparse_softmax_cross_entropy_with_logits(
