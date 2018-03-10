4541fc628affebef90645f249ba230049f15d562
Guillaume Klein <guillaumekln@users.noreply.github.com>
2018-03-10 11:17:34 +0100

    Use train_and_eval in WMT training scripts

--- scripts/wmt/run_wmt_ende.sh
+++ scripts/wmt/run_wmt_ende.sh
@@ -2,7 +2,7 @@
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
-PYTHONPATH=../.. python -m bin.main train \
+PYTHONPATH=../.. python -m bin.main train_and_eval \
   --model ../../config/models/transformer.py \
   --config config/wmt_ende.yml \
   --num_gpus 4
