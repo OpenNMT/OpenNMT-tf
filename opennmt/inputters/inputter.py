Fix dataset size computation for parallel inputs

opennmt/inputters/inputter.py
@@ -241,8 +241,9 @@ class MultiInputter(Inputter):
   def make_dataset(self, data_file):
     raise NotImplementedError()
 
+  @abc.abstractmethod
   def get_dataset_size(self, data_file):
-    return self.inputters[0].get_dataset_size(data_file)
+    raise NotImplementedError()
 
   def initialize(self, metadata):
     for inputter in self.inputters:
@@ -296,6 +297,18 @@ class ParallelInputter(MultiInputter):
         inputter.make_dataset(data)
         for inputter, data in zip(self.inputters, data_file)]
     return tf.data.Dataset.zip(tuple(datasets))
+
+  def get_dataset_size(self, data_file):
+    if not isinstance(data_file, list) or len(data_file) != len(self.inputters):
+      raise ValueError("The number of data files must be the same as the number of inputters")
+    dataset_sizes = [
+        inputter.get_dataset_size(data)
+        for inputter, data in zip(self.inputters, data_file)]
+    dataset_size = dataset_sizes[0]
+    for size in dataset_sizes:
+      if size != dataset_size:
+        raise RuntimeError("The parallel data files do not have the same size")
+    return dataset_size
 
   def _get_serving_input(self):
     all_receiver_tensors = {}
@@ -362,6 +375,9 @@ class MixedInputter(MultiInputter):
 
   def make_dataset(self, data_file):
     return self.inputters[0].make_dataset(data_file)
+
+  def get_dataset_size(self, data_file):
+    return self.inputters[0].get_dataset_size(data_file)
 
   def _get_serving_input(self):
     all_receiver_tensors = {}

opennmt/tests/inputter_test.py
@@ -120,6 +120,7 @@ class TextInputterTest(tf.test.TestCase):
 
     embedder = text_inputter.WordEmbedder(
         "vocabulary_file", embedding_size=10)
+    self.assertEqual(1, embedder.get_dataset_size(data_file))
     data, transformed = _first_element(
         embedder, data_file, {"vocabulary_file": vocab_file})
 
@@ -155,6 +156,7 @@ class TextInputterTest(tf.test.TestCase):
       data.write("hello world !\n")
 
     embedder = text_inputter.CharConvEmbedder("vocabulary_file", 10, 5)
+    self.assertEqual(1, embedder.get_dataset_size(data_file))
     data, transformed = _first_element(
         embedder, data_file, {"vocabulary_file": vocab_file})
 
@@ -190,13 +192,16 @@ class TextInputterTest(tf.test.TestCase):
     with open(data_file, "w") as data:
       data.write("hello world !\n")
 
+    data_files = [data_file, data_file]
+
     parallel_inputter = inputter.ParallelInputter([
         text_inputter.WordEmbedder("vocabulary_file_1", embedding_size=10),
         text_inputter.WordEmbedder("vocabulary_file_2", embedding_size=5)])
+    self.assertEqual(1, parallel_inputter.get_dataset_size(data_files))
 
     data, transformed = _first_element(
         parallel_inputter,
-        [data_file, data_file],
+        data_files,
         {"vocabulary_file_1": vocab_file, "vocabulary_file_2": vocab_file})
 
     self.assertEqual(2, len(parallel_inputter.get_length(data)))
@@ -242,6 +247,7 @@ class TextInputterTest(tf.test.TestCase):
         text_inputter.WordEmbedder("vocabulary_file_1", embedding_size=10),
         text_inputter.CharConvEmbedder("vocabulary_file_2", 10, 5)],
         reducer=reducer.ConcatReducer())
+    self.assertEqual(1, mixed_inputter.get_dataset_size(data_file))
 
     data, transformed = _first_element(
         mixed_inputter,
@@ -275,6 +281,7 @@ class RecordInputterTest(tf.test.TestCase):
     writer.close()
 
     inputter = record_inputter.SequenceRecordInputter()
+    self.assertEqual(1, inputter.get_dataset_size(record_file))
     data, transformed = _first_element(inputter, record_file)
     input_receiver = inputter.get_serving_input_receiver()
 