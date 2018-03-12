OpenNMT/OpenNMT-tf

commit f59f011a8cc84b025b8d260503968cf33f650bdc

Keep known static shape in {split,combine}_heads

opennmt/layers/transformer.py
@@ -100,9 +100,12 @@ def split_heads(inputs, num_heads):
   Returns:
     A ``tf.Tensor`` of shape :math:`[B, H, T, D / H]`.
   """
-  inputs = tf.reshape(inputs, [tf.shape(inputs)[0], tf.shape(inputs)[1], num_heads, -1])
-  inputs = tf.transpose(inputs, perm=[0, 2, 1, 3])
-  return inputs
+  static_shape = inputs.get_shape().as_list()
+  depth = static_shape[-1]
+  outputs = tf.reshape(
+      inputs, [tf.shape(inputs)[0], tf.shape(inputs)[1], num_heads, depth // num_heads])
+  outputs = tf.transpose(outputs, perm=[0, 2, 1, 3])
+  return outputs
 
 def combine_heads(inputs):
   """Concatenates heads.
@@ -113,9 +116,12 @@ def combine_heads(inputs):
   Returns:
     A ``tf.Tensor`` of shape :math:`[B, T, D * H]`.
   """
-  inputs = tf.transpose(inputs, perm=[0, 2, 1, 3])
-  inputs = tf.reshape(inputs, [tf.shape(inputs)[0], tf.shape(inputs)[1], -1])
-  return inputs
+  static_shape = inputs.get_shape().as_list()
+  depth = static_shape[-1]
+  num_heads = static_shape[1]
+  outputs = tf.transpose(inputs, perm=[0, 2, 1, 3])
+  outputs = tf.reshape(inputs, [tf.shape(outputs)[0], tf.shape(outputs)[1], depth * num_heads])
+  return outputs
 
 def dot_product_attention(queries,
                           keys,
@@ -225,7 +231,6 @@ def multi_head_attention(num_heads,
 
   # Concatenate all heads output.
   combined = combine_heads(heads)
-  combined.set_shape((None, None, num_units))
   outputs = tf.layers.conv1d(combined, num_units, 1)
 
   return outputs

opennmt/tests/transformer_test.py
@@ -105,6 +105,43 @@ class TransformerTest(tf.test.TestCase):
       for b in range(len(length)):
         self.assertAllEqual(expected, mask[b])
 
+  def testSplitHeads(self):
+    batch_size = 3
+    length = [5, 3, 7]
+    num_heads = 8
+    depth = 20
+
+    inputs = tf.placeholder_with_default(
+        np.random.randn(batch_size, max(length), depth * num_heads).astype(np.float32),
+        shape=(None, None, depth * num_heads))
+    outputs = transformer.split_heads(inputs, num_heads)
+
+    static_shape = outputs.get_shape().as_list()
+    self.assertEqual(num_heads, static_shape[1])
+    self.assertEqual(depth, static_shape[-1])
+
+    with self.test_session() as sess:
+      outputs = sess.run(outputs)
+      self.assertAllEqual([batch_size, num_heads, max(length), depth], outputs.shape)
+
+  def testCombineHeads(self):
+    batch_size = 3
+    length = [5, 3, 7]
+    num_heads = 8
+    depth = 20
+
+    inputs = tf.placeholder_with_default(
+        np.random.randn(batch_size, num_heads, max(length), depth).astype(np.float32),
+        shape=(None, num_heads, None, depth))
+    outputs = transformer.combine_heads(inputs)
+
+    static_shape = outputs.get_shape().as_list()
+    self.assertEqual(depth * num_heads, static_shape[-1])
+
+    with self.test_session() as sess:
+      outputs = sess.run(outputs)
+      self.assertAllEqual([batch_size, max(length), depth * num_heads], outputs.shape)
+
   def testScaledDotAttention(self):
     batch_size = 3
     num_heads = 8

commit 40ce318368a275f98f95662274f1f5f0948a44e5

Fix regression introduced in f59f011a
combine_heads were applied on the wrong dimension.

opennmt/layers/transformer.py
@@ -120,7 +120,7 @@ def combine_heads(inputs):
   depth = static_shape[-1]
   num_heads = static_shape[1]
   outputs = tf.transpose(inputs, perm=[0, 2, 1, 3])
-  outputs = tf.reshape(inputs, [tf.shape(outputs)[0], tf.shape(outputs)[1], depth * num_heads])
+  outputs = tf.reshape(outputs, [tf.shape(outputs)[0], tf.shape(outputs)[1], depth * num_heads])
   return outputs
 
 def dot_product_attention(queries,

opennmt/tests/transformer_test.py
@@ -142,6 +142,22 @@ class TransformerTest(tf.test.TestCase):
       outputs = sess.run(outputs)
       self.assertAllEqual([batch_size, max(length), depth * num_heads], outputs.shape)
 
+  def testSplitAndCombineHeads(self):
+    batch_size = 3
+    length = [5, 3, 7]
+    num_heads = 8
+    depth = 20
+
+    inputs = tf.placeholder_with_default(
+        np.random.randn(batch_size, max(length), depth * num_heads).astype(np.float32),
+        shape=(None, None, depth * num_heads))
+    split = transformer.split_heads(inputs, num_heads)
+    combined = transformer.combine_heads(split)
+
+    with self.test_session() as sess:
+      inputs, combined = sess.run([inputs, combined])
+      self.assertAllEqual(inputs, combined)
+
   def testScaledDotAttention(self):
     batch_size = 3
     num_heads = 8