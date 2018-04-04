284d6a77a09cd6d51055511f212dbc27e41de6ff
Guillaume Klein <guillaume.klein@systrangroup.com>
2018-04-04 18:21:41 +0200

    Prepare setup.py for PyPi release

--- setup.py
+++ setup.py
@@ -4,18 +4,47 @@ setup(
     name="OpenNMT-tf",
     version="0.1",
     license="MIT",
+    description="Neural machine translation and sequence learning using TensorFlow",
+    author="OpenNMT",
+    author_email="guillaume.klein@opennmt.net",
+    url="http://opennmt.net",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2"
+        "Programming Language :: Python :: 2.7"
+        "Programming Language :: Python :: 3"
+        "Programming Language :: Python :: 3.4"
+        "Programming Language :: Python :: 3.5"
+        "Programming Language :: Python :: 3.6"
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    project_urls={
+        "Documentation": "http://opennmt.net/OpenNMT-tf/",
+        "Forum": "http://forum.opennmt.net/",
+        "Gitter": "https://gitter.im/OpenNMT/OpenNMT-tf",
+        "Source": "https://github.com/OpenNMT/OpenNMT-tf/"
+    },
+    keywords="tensorflow opennmt nmt neural machine translation",
     install_requires=[
         "pyyaml"
     ],
     extras_require={
-        "tensorflow": ["tensorflow==1.6.0"],
-        "tensorflow_gpu": ["tensorflow-gpu==1.6.0"]
+        "tensorflow": ["tensorflow>=1.6.0"],
+        "tensorflow_gpu": ["tensorflow-gpu>=1.6.0"]
     },
     tests_require=[
         "nose2"
     ],
     test_suite="nose2.collector.collector",
     packages=find_packages(exclude=["bin", "*.tests"]),
+    package_data={
+        "opennmt": [
+            "../third_party/multi-bleu.perl",
+            "../third_party/multi-bleu-detok.perl"]
+    },
     entry_points={
         "console_scripts": [
             "onmt-ark-to-records=opennmt.bin.ark_to_records:main",
