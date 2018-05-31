fc3c646efe826a6a5ebe036b83178acd4fd3fbbf
Guillaume Klein <guillaumekln@users.noreply.github.com>
2018-05-31 10:26:35 +0200

    Adam optimizer that accumulates gradients of multiple steps (#141)

--- opennmt/optimizers/__init__.py
+++ opennmt/optimizers/__init__.py
@@ -3,3 +3,5 @@
 from opennmt.optimizers.adafactor import AdafactorOptimizer
 from opennmt.optimizers.adafactor import get_optimizer_from_params \
     as get_adafactor_optimizer_from_params
+
+from opennmt.optimizers.multistep_adam import MultistepAdamOptimizer
