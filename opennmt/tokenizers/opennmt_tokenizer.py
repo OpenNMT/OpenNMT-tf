d42032d825201b5eee463fbaa7fb15ac46ed0643
Guillaume Klein <guillaume.klein@systrangroup.com>
2018-07-03 17:08:27 +0200

    Make code forward compatible with future pyonmttok options

--- opennmt/tokenizers/opennmt_tokenizer.py
+++ opennmt/tokenizers/opennmt_tokenizer.py
@@ -1,6 +1,6 @@
 """Define the OpenNMT tokenizer."""
 
-import six
+import copy
 
 import tensorflow as tf
 
@@ -18,28 +18,12 @@ def create_tokenizer(config):
   Returns:
     A ``pyonmttok.Tokenizer``.
   """
-  def _set(kwargs, key):
-    if key in config:
-      value = config[key]
-      if isinstance(value, six.string_types):
-        value = tf.compat.as_bytes(value)
-      kwargs[key] = value
-
-  kwargs = {}
-  _set(kwargs, "bpe_model_path")
-  _set(kwargs, "sp_model_path")
-  _set(kwargs, "joiner")
-  _set(kwargs, "joiner_annotate")
-  _set(kwargs, "joiner_new")
-  _set(kwargs, "spacer_annotate")
-  _set(kwargs, "case_feature")
-  _set(kwargs, "no_substitution")
-  _set(kwargs, "segment_case")
-  _set(kwargs, "segment_numbers")
-  _set(kwargs, "segment_alphabet_change")
-  _set(kwargs, "segment_alphabet")
-
-  return pyonmttok.Tokenizer(config.get("mode", "conservative"), **kwargs)
+  kwargs = copy.deepcopy(config)
+  mode = "conservative"
+  if "mode" in kwargs:
+    mode = kwargs["mode"]
+    del kwargs["mode"]
+  return pyonmttok.Tokenizer(mode, **kwargs)
 
 
 class OpenNMTTokenizer(Tokenizer):
