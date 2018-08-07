bbe783890ed4eab7086bc502aa80fc55a35a9030
Guillaume Klein <guillaume.klein@systrangroup.com>
2018-08-07 13:09:49 +0200

    Add argument to SequenceToSequence models to share embeddings
    Start by supporting simple source and target sharing.

--- opennmt/models/__init__.py
+++ opennmt/models/__init__.py
@@ -1,6 +1,6 @@
 """Module defining models."""
 
 from opennmt.models.transformer import Transformer
-from opennmt.models.sequence_to_sequence import SequenceToSequence
+from opennmt.models.sequence_to_sequence import SequenceToSequence, EmbeddingsSharingLevel
 from opennmt.models.sequence_tagger import SequenceTagger
 from opennmt.models.sequence_classifier import SequenceClassifier
