cabebb578881c6eca945f07437ceaffcde7be2e2
Guillaume Klein <guillaume.klein@systrangroup.com>
2018-08-13 14:27:57 +0200

    Remove unused top-level bin/ directory

--- bin/ark_to_records.py
+++
@@ -1,3 +0,0 @@
-if __name__ == "__main__":
-  from opennmt.bin import ark_to_records
-  ark_to_records.main()
--- bin/merge_config.py
+++
@@ -1,3 +0,0 @@
-if __name__ == "__main__":
-  from opennmt.bin import merge_config
-  merge_config.main()
