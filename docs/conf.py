2d399e3890ad388d2e2bbebab31e8b1c957789a0
Guillaume Klein <guillaume.klein@systrangroup.com>
2019-01-21 10:15:00 +0100

    Bump version

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ author = "OpenNMT"
 language = "en"
 
 version = "1.17"  # The short X.Y version.
-release = "1.17.0"  # The full version, including alpha/beta/rc tags.
+release = "1.17.1"  # The full version, including alpha/beta/rc tags.
 
 source_suffix = ".rst"
 master_doc = "index"
