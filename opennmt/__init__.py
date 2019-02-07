13eccafa459e5421e01b2301ca2245134681e27d
Guillaume Klein <guillaume.klein@systrangroup.com>
2019-02-07 10:42:06 +0100

    Add v2 module for TensorFlow 2.0 compatibility

--- opennmt/__init__.py
+++ opennmt/__init__.py
@@ -1,12 +1,18 @@
 """OpenNMT module."""
 
+import tensorflow as tf
+
+
 __version__ = "1.18.0"
 
-from opennmt import decoders
-from opennmt import encoders
-from opennmt import inputters
-from opennmt import layers
-from opennmt import models
-from opennmt import tokenizers
+if tf.__version__.startswith("2"):
+  from opennmt.v2 import *  # pylint: disable=wildcard-import
+else:
+  from opennmt import decoders
+  from opennmt import encoders
+  from opennmt import inputters
+  from opennmt import layers
+  from opennmt import models
+  from opennmt import tokenizers
 
-from opennmt.runner import Runner
+  from opennmt.runner import Runner
---
+++ opennmt/v2/__init__.py
@@ -0,0 +1,3 @@
+"""Module for TensorFlow 2.0 compatibility."""
+
+from opennmt.v2 import layers
