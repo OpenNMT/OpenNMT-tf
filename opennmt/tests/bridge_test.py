78b8c4b90cea0ccfe215c2876eefee23d8acecd1
Guillaume Klein <guillaume.klein@systrangroup.com>
2019-02-27 11:28:51 +0100

    Run Bridge tests in TF2

--- opennmt/tests/bridge_test.py
+++ opennmt/tests/bridge_test.py
@@ -1,29 +1,27 @@
 import tensorflow as tf
 
 from opennmt.layers import bridge
-from opennmt.tests import test_util
 
 
 def _build_state(num_layers, num_units, batch_size):
-  cell = tf.nn.rnn_cell.MultiRNNCell(
-      [tf.nn.rnn_cell.LSTMCell(num_units) for _ in range(num_layers)])
-  return cell.zero_state(batch_size, tf.float32)
+  return [
+      [tf.zeros([batch_size, num_units]), tf.zeros([batch_size, num_units])]
+      for _ in range(num_layers)]
 
 
-@test_util.run_tf1_only
 class BridgeTest(tf.test.TestCase):
 
   def testZeroBridge(self):
     encoder_state = _build_state(4, 20, 6)
     decoder_state = _build_state(3, 60, 6)
     state = bridge.ZeroBridge()(encoder_state, decoder_state)
-    self.assertTupleEqual(decoder_state, state)
+    self.assertAllEqual(decoder_state, state)
 
   def testCopyBridge(self):
     encoder_state = _build_state(3, 20, 6)
     decoder_state = _build_state(3, 20, 6)
     state = bridge.CopyBridge()(encoder_state, decoder_state)
-    self.assertTupleEqual(encoder_state, state)
+    self.assertAllEqual(encoder_state, state)
 
   def testCopyBridgeLayerMismatch(self):
     encoder_state = _build_state(3, 20, 6)
