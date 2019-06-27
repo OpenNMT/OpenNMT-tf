7e9a9f0d0400c6f25fd9680878b8135b78923466
Guillaume Klein <guillaume.klein@systrangroup.com>
2019-06-27 15:39:58 +0200

    Move regularization penalty to losses.py

--- opennmt/tests/losses_test.py
+++ opennmt/tests/losses_test.py
@@ -2,10 +2,10 @@ from parameterized import parameterized
 
 import tensorflow as tf
 
-from opennmt.utils import optim
+from opennmt.utils import losses
 
 
-class OptimTest(tf.test.TestCase):
+class LossesTest(tf.test.TestCase):
 
   @parameterized.expand([
       ["l1", 1e-4],
@@ -17,18 +17,18 @@ class OptimTest(tf.test.TestCase):
   def testRegularization(self, type, scale):
     layer = tf.keras.layers.Dense(256)
     layer.build([None, 128])
-    regularization = optim.regularization_penalty(
+    regularization = losses.regularization_penalty(
         type, scale, layer.trainable_variables)
     self.assertEqual(0, len(regularization.shape.as_list()))
     self.evaluate(regularization)
 
   def testRegulaizationInvalidType(self):
     with self.assertRaises(ValueError):
-      optim.regularization_penalty("l3", 1e-4, [])
+      losses.regularization_penalty("l3", 1e-4, [])
 
   def testRegulaizationMissingScaleValue(self):
     with self.assertRaises(ValueError):
-      optim.regularization_penalty("l1_l2", 1e-4, [])
+      losses.regularization_penalty("l1_l2", 1e-4, [])
 
 
 if __name__ == "__main__":
--- opennmt/utils/losses.py
+++ opennmt/utils/losses.py
@@ -79,3 +79,40 @@ def cross_entropy_loss(logits,
   loss = tf.reduce_sum(cross_entropy)
   loss_normalizer = tf.cast(tf.shape(cross_entropy)[0], loss.dtype)
   return loss, loss_normalizer
+
+def regularization_penalty(regularization_type, scale, weights):
+  """Computes the weights regularization penalty.
+
+  Args:
+    regularization_type: The regularization type: ``l1``, ``l2``, or ``l1_l2``.
+    scale: The regularization multiplier. If :obj:`regularization_type` is
+      ``l1_l2``, this should be a list or tuple containing the L1 regularization
+      scale and the L2 regularization scale.
+    weights: The list of weights.
+
+  Returns:
+    The regularization penalty.
+
+  Raises:
+    ValueError: if :obj:`regularization_type` is invalid or is ``l1_l2`` but
+      :obj:`scale` is not a sequence.
+  """
+  regularization_type = regularization_type.lower()
+  if regularization_type == "l1":
+    regularizer = tf.keras.regularizers.l1(l=float(scale))
+  elif regularization_type == "l2":
+    regularizer = tf.keras.regularizers.l2(l=float(scale))
+  elif regularization_type == "l1_l2":
+    if not isinstance(scale, (list, tuple)) or len(scale) != 2:
+      raise ValueError("l1_l2 regularization requires 2 scale values")
+    regularizer = tf.keras.regularizers.l1_l2(
+        l1=float(scale[0]), l2=float(scale[1]))
+  else:
+    raise ValueError("invalid regularization type %s" % regularization_type)
+
+  weights = list(filter(lambda v: not _is_bias(v), weights))
+  penalty = tf.add_n([regularizer(w) for w in weights])
+  return penalty
+
+def _is_bias(variable):
+  return len(variable.shape.as_list()) == 1 and variable.name.endswith("bias:0")
