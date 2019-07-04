Fix checkpoint averaging of model with nested structures

opennmt/models/model.py
@@ -9,6 +9,7 @@ import tensorflow as tf
 
 from opennmt import optimizers
 from opennmt import schedules
+from opennmt.utils import misc
 
 
 @six.add_metaclass(abc.ABCMeta)
@@ -50,7 +51,7 @@ class Model(tf.keras.layers.Layer):
         if not isinstance(freeze_layers, list):
           freeze_layers = [freeze_layers]
         for layer_path in freeze_layers:
-          layer = _layer_from_path(self, layer_path)
+          layer = misc.index_structure(self, layer_path)
           layer.trainable = False
           self._frozen_layers.append(layer)
     return super(Model, self).trainable_weights
@@ -266,15 +267,3 @@ class Model(tf.keras.layers.Layer):
     """
     _ = params
     print(prediction, file=stream)
-
-
-def _layer_from_path(layer, path):
-  for key in path.split("/"):
-    try:
-      index = int(key)
-      layer = layer[index] if index < len(layer) else None
-    except ValueError:
-      layer = getattr(layer, key, None)
-    if layer is None:
-      raise ValueError("Invalid layer path: %s" % path)
-  return layer

opennmt/tests/checkpoint_test.py
@@ -12,11 +12,13 @@ from opennmt.utils import checkpoint as checkpoint_util
 class _DummyModel(tf.keras.layers.Layer):
 
   def __init__(self):
-      super(_DummyModel, self).__init__()
-      self.dense = tf.keras.layers.Dense(20)
+    super(_DummyModel, self).__init__()
+    self.layers = [tf.keras.layers.Dense(20), tf.keras.layers.Dense(20)]
 
   def call(self, x):
-      return self.dense(x)
+    for layer in self.layers:
+      x = layer(x)
+    return x
 
 
 class CheckpointTest(tf.test.TestCase):
@@ -86,8 +88,8 @@ class CheckpointTest(tf.test.TestCase):
     checkpoint_manager = tf.train.CheckpointManager(
         checkpoint, directory, max_to_keep=num_checkpoints)
     for step in steps:
-      _assign_var(model.dense.kernel, step)
-      _assign_var(model.dense.bias, step)
+      _assign_var(model.layers[0].kernel, step)
+      _assign_var(model.layers[0].bias, step)
       checkpoint_manager.save(checkpoint_number=step)
 
     output_dir = os.path.join(self.get_temp_dir(), "dst")
@@ -96,8 +98,8 @@ class CheckpointTest(tf.test.TestCase):
     avg_checkpoint = tf.train.latest_checkpoint(output_dir)
     self.assertIsNotNone(avg_checkpoint)
     checkpoint.restore(avg_checkpoint)
-    self.assertTrue(_all_equal(model.dense.kernel, avg_value))
-    self.assertTrue(_all_equal(model.dense.bias, avg_value))
+    self.assertTrue(_all_equal(model.layers[0].kernel, avg_value))
+    self.assertTrue(_all_equal(model.layers[0].bias, avg_value))
     self.assertListEqual(
         _get_var_list(avg_checkpoint),
         _get_var_list(checkpoint_manager.latest_checkpoint))

opennmt/utils/checkpoint.py
@@ -6,6 +6,8 @@ import six
 import tensorflow as tf
 import numpy as np
 
+from opennmt.utils import misc
+
 
 class Checkpoint(object):
   """Wrapper around TensorFlow checkpoints utilities."""
@@ -233,21 +235,11 @@ def average_checkpoints(model_dir,
       for path in six.iterkeys(reader.get_variable_to_shape_map()):
         if not path.startswith(model_key) or ".OPTIMIZER_SLOT" in path:
           continue
-        variable = _get_variable_from_path(trackables, path)
-        if variable is None:
-          continue
+        variable_path = path.replace("/.ATTRIBUTES/VARIABLE_VALUE", "")
+        variable = misc.index_structure(trackables, variable_path)
         value = reader.get_tensor(path)
         variable.assign_add(value / num_checkpoints)
 
   new_checkpoint_manager = tf.train.CheckpointManager(checkpoint, output_dir, max_to_keep=None)
   new_checkpoint_manager.save(checkpoint_number=last_step)
   return output_dir
-
-def _get_variable_from_path(trackables, path):
-  fields = path.split("/")
-  value = trackables[fields[0]]
-  for key in fields[1:]:
-    value = getattr(value, key, None)
-    if value is None or isinstance(value, tf.Variable):
-      return value
-  return None