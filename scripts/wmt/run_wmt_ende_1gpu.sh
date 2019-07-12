3877fb11c971c038d8c2c547eaf34d653bf0bd55
Guillaume Klein <guillaume.klein@systrangroup.com>
2019-07-12 11:46:19 +0200

    Quick update of WMT script

--- scripts/wmt/run_wmt_ende_1gpu.sh
+++ scripts/wmt/run_wmt_ende_1gpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-onmt-main \
-          --model_type Transformer \
-          --config config/wmt_ende.yml --auto_config train_and_eval
+onmt-main --model_type Transformer \
+          --config config/wmt_ende.yml --auto_config \
+          train --with_eval
