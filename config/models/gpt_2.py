07556535607890ec37e19649102d1350b7f85676
Guillaume Klein <guillaume.klein@systrangroup.com>
2019-07-12 11:04:08 +0200

    Update custom models

--- config/models/gpt_2.py
+++ config/models/gpt_2.py
@@ -20,24 +20,24 @@ class GPT2Small(onmt.models.LanguageModel):
 
   def __init__(self):
     super(GPT2Small, self).__init__(
-        decoder=onmt.decoders.SelfAttentionDecoderV2(
+        decoder=onmt.decoders.SelfAttentionDecoder(
             num_layers=12,
             num_units=768,
             num_heads=12,
             ffn_inner_dim=3072,
             ffn_activation=gelu,
-            position_encoder=onmt.layers.PositionEmbedder(maximum_position=1024),
+            position_encoder_class=lambda: onmt.layers.PositionEmbedder(maximum_position=1024),
             num_sources=0),
         embedding_size=768)
 
-  def auto_config(self, num_devices=1):
-    config = super(GPT2Small, self).auto_config(num_devices=num_devices)
+  def auto_config(self, num_replicas=1):
+    config = super(GPT2Small, self).auto_config(num_replicas=num_replicas)
     return misc.merge_dict(config, {
         "params": {
             "average_loss_in_time": True,
-            "optimizer": "AdamOptimizer",
+            "optimizer": "Adam",
             "learning_rate": 2.5e-4,
-            "decay_type": "cosine_annealing",
+            "decay_type": "CosineAnnealing",
             "decay_params": {
                 "max_step": 1000000,
                 "warmup_steps": 2000,
--- config/models/multi_features_nmt.py
+++ config/models/multi_features_nmt.py
@@ -1,40 +1,47 @@
 """Defines a sequence to sequence model with multiple input features. For
 example, this could be words, parts of speech, and lemmas that are embedded in
-parallel and concatenated into a single input embedding. The features are
-separate data files with separate vocabularies.
+parallel and concatenated into a single input embedding.
+
+The features are separate data files with separate vocabularies. The YAML
+configuration file should look like this:
+
+data:
+  train_features_file:
+    - features_1.txt
+    - features_2.txt
+    - features_3.txt
+  train_labels_file: target.txt
+  source_1_vocabulary: feature_1_vocab.txt
+  source_2_vocabulary: feature_2_vocab.txt
+  source_3_vocabulary: feature_3_vocab.txt
+  target_vocabulary: target_vocab.txt
 """
 
 import tensorflow as tf
+import tensorflow_addons as tfa
 import opennmt as onmt
 
 def model():
   return onmt.models.SequenceToSequence(
       source_inputter=onmt.inputters.ParallelInputter([
-          onmt.inputters.WordEmbedder(
-              vocabulary_file_key="source_words_vocabulary",
-              embedding_size=512),
-          onmt.inputters.WordEmbedder(
-              vocabulary_file_key="feature_1_vocabulary",
-              embedding_size=16),
-          onmt.inputters.WordEmbedder(
-              vocabulary_file_key="feature_2_vocabulary",
-              embedding_size=64)],
+          onmt.inputters.WordEmbedder(embedding_size=512),
+          onmt.inputters.WordEmbedder(embedding_size=16),
+          onmt.inputters.WordEmbedder(embedding_size=64)],
           reducer=onmt.layers.ConcatReducer()),
-      target_inputter=onmt.inputters.WordEmbedder(
-          vocabulary_file_key="target_words_vocabulary",
-          embedding_size=512),
-      encoder=onmt.encoders.BidirectionalRNNEncoder(
+      target_inputter=onmt.inputters.WordEmbedder(embedding_size=512),
+      encoder=onmt.encoders.RNNEncoder(
           num_layers=4,
-          num_units=512,
-          reducer=onmt.layers.ConcatReducer(),
-          cell_class=tf.contrib.rnn.LSTMCell,
+          num_units=256,
+          bidrectional=True,
           dropout=0.3,
-          residual_connections=False),
+          residual_connections=False,
+          reducer=onmt.layers.ConcatReducer(),
+          cell_class=tf.keras.layers.LSTMCell),
       decoder=onmt.decoders.AttentionalRNNDecoder(
           num_layers=4,
           num_units=512,
           bridge=onmt.layers.CopyBridge(),
-          attention_mechanism_class=tf.contrib.seq2seq.LuongAttention,
-          cell_class=tf.contrib.rnn.LSTMCell,
+          attention_mechanism_class=tfa.seq2seq.LuongAttention,
+          cell_class=tf.keras.layers.LSTMCell,
           dropout=0.3,
           residual_connections=False))
