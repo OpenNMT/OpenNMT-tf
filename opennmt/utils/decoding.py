Ensure correct broadcasting with tf.where_v2

opennmt/utils/decoding.py
@@ -368,7 +368,10 @@ def dynamic_decode(symbols_to_logits_fn,
     logits = tf.cond(
         step < minimum_iterations,
         true_fn=lambda: _penalize_token(logits, end_id),
-        false_fn=lambda: tf.where(tf.expand_dims(finished, -1), x=eos_max_prob, y=logits))
+        false_fn=lambda: tf.where(
+            tf.broadcast_to(tf.expand_dims(finished, -1), tf.shape(logits)),
+            x=eos_max_prob,
+            y=logits))
     log_probs = tf.nn.log_softmax(logits)
 
     # Run one decoding strategy step.