216553203df0f6ab5c9af2b7b46255dbfb69a04c
Guillaume Klein <guillaume.klein@systrangroup.com>
2019-10-10 12:06:12 +0200

    Cleanup tests to directly check the RaggedTensor output

--- opennmt/tests/text_test.py
+++ opennmt/tests/text_test.py
@@ -9,51 +9,42 @@ from opennmt.data import text
 
 class TextTest(tf.test.TestCase):
 
-  def _testTokensToChars(self, tokens, expected_chars, expected_lengths):
+  def _testTokensToChars(self, tokens, expected_chars):
     expected_chars = tf.nest.map_structure(tf.compat.as_bytes, expected_chars)
     chars = text.tokens_to_chars(tf.constant(tokens, dtype=tf.string))
-    chars, lengths = chars.to_tensor(), chars.row_lengths()
-    chars, lengths = self.evaluate([chars, lengths])
-    self.assertListEqual(expected_chars, chars.tolist())
-    self.assertListEqual(expected_lengths, lengths.tolist())
+    self.assertListEqual(chars.to_list(), expected_chars)
 
   def testTokensToCharsEmpty(self):
-    self._testTokensToChars([], [], [])
+    self._testTokensToChars([], [])
 
   def testTokensToCharsSingle(self):
-    self._testTokensToChars(["Hello"], [["H", "e", "l", "l", "o"]], [5])
+    self._testTokensToChars(["Hello"], [["H", "e", "l", "l", "o"]])
 
   def testTokensToCharsMixed(self):
     self._testTokensToChars(
-        ["Just", "a", "测试"],
-        [["J", "u", "s", "t"], ["a", "", "", ""], ["测", "试", "", ""]],
-        [4, 1, 2])
+        ["Just", "a", "测试"], [["J", "u", "s", "t"], ["a"], ["测", "试"]])
 
   @parameterized.expand([
-    [["a￭", "b", "c￭", "d", "￭e"], [["a￭", "b", ""], ["c￭", "d", "￭e"]]],
-    [["a", "￭", "b", "c￭", "d", "￭", "e"], [["a", "￭", "b", ""], ["c￭", "d", "￭", "e"]]],
+    [["a￭", "b", "c￭", "d", "￭e"], [["a￭", "b"], ["c￭", "d", "￭e"]]],
+    [["a", "￭", "b", "c￭", "d", "￭", "e"], [["a", "￭", "b"], ["c￭", "d", "￭", "e"]]],
   ])
   def testToWordsWithJoiner(self, tokens, expected):
+    expected = tf.nest.map_structure(tf.compat.as_bytes, expected)
     tokens = tf.constant(tokens)
-    expected = tf.constant(expected)
     words = text.tokens_to_words(tokens)
-    words = words.to_tensor()
-    words, expected = self.evaluate([words, expected])
-    self.assertAllEqual(words, expected)
+    self.assertAllEqual(words.to_list(), expected)
 
   @parameterized.expand([
-    [["▁a", "b", "▁c", "d", "e"], [["▁a", "b", ""], ["▁c", "d", "e"]]],
-    [["▁", "a", "b", "▁", "c", "d", "e"], [["▁", "a", "b", ""], ["▁", "c", "d", "e"]]],
-    [["a▁", "b", "c▁", "d", "e"], [["a▁", ""], ["b", "c▁"], ["d", "e"]]],
-    [["a", "▁b▁", "c", "d", "▁", "e"], [["a", ""], ["▁b▁", ""], ["c", "d"], ["▁", "e"]]],
+    [["▁a", "b", "▁c", "d", "e"], [["▁a", "b"], ["▁c", "d", "e"]]],
+    [["▁", "a", "b", "▁", "c", "d", "e"], [["▁", "a", "b"], ["▁", "c", "d", "e"]]],
+    [["a▁", "b", "c▁", "d", "e"], [["a▁"], ["b", "c▁"], ["d", "e"]]],
+    [["a", "▁b▁", "c", "d", "▁", "e"], [["a"], ["▁b▁"], ["c", "d"], ["▁", "e"]]],
   ])
   def testToWordsWithSpacer(self, tokens, expected):
+    expected = tf.nest.map_structure(tf.compat.as_bytes, expected)
     tokens = tf.constant(tokens)
-    expected = tf.constant(expected)
     words = text.tokens_to_words(tokens, subword_token="▁", is_spacer=True)
-    words = words.to_tensor()
-    words, expected = self.evaluate([words, expected])
-    self.assertAllEqual(words, expected)
+    self.assertAllEqual(words.to_list(), expected)
 
   def _testPharaohAlignments(self, line, lengths, expected_matrix):
     matrix = text.alignment_matrix_from_pharaoh(
