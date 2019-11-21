0564912e17698d280e0cfae6dcd4862c7c8a7ff6
Guillaume Klein <guillaumekln@users.noreply.github.com>
2019-11-21 10:48:16 +0100

    Add function to convert V1 configuration to V2 (#553)

--- opennmt/config.py
+++ opennmt/config.py
@@ -2,6 +2,7 @@
 
 from importlib import import_module
 
+import copy
 import os
 import sys
 import tensorflow as tf
@@ -143,3 +144,132 @@ def load_config(config_paths, config=None):
       merge_dict(config, subconfig)
 
   return config
+
+
+def convert_to_v2_config(v1_config):
+  """Converts a V1 configuration to its V2 equivalent.
+
+  Args:
+    v1_config: The V1 configuration.
+
+  Returns:
+    The V2 configuration.
+
+  Raises:
+    ValueError: if the conversion can not be done automatically.
+  """
+  config = copy.deepcopy(v1_config)
+
+  _convert_to_v2_params(config)
+
+  data_config = config.get("data")
+  if data_config:
+    # Covers most of seq2seq models in the catalog.
+    _rename_opt(data_config, "source_words_vocabulary", "source_vocabulary")
+    _rename_opt(data_config, "target_words_vocabulary", "target_vocabulary")
+
+  for section_name in ("train", "eval", "infer", "score", "params"):
+    section = config.get(section_name)
+    if section is None:
+      continue
+
+    _delete_opt(section, "num_threads")
+    _delete_opt(section, "prefetch_buffer_size")
+    _rename_opt(section, "bucket_width", "length_bucket_width")
+    if section_name == "train":
+      _rename_opt(section, "train_steps", "max_step")
+      _delete_opt(section, "save_checkpoints_secs")
+    elif section_name == "eval":
+      _delete_opt(section, "eval_delay")
+      _delete_opt(section, "exporters")
+
+    # Remove empty sections.
+    if not section:
+      config.pop(section_name)
+
+  return config
+
+def _convert_to_v2_params(config):
+  params = config.get("params")
+  if not params:
+    return
+  if "freeze_variables" in params:
+    raise ValueError("params/freeze_variables should be manually converted to "
+                     "params/freeze_layers")
+
+  _convert_to_v2_optimizer(params)
+  _convert_to_v2_lr_schedules(params)
+  _convert_to_v2_step_accumulation(params, config)
+  _delete_opt(params, "param_init")
+  _delete_opt(params, "loss_scale")
+  _delete_opt(params, "horovod")
+  _delete_opt(params, "maximum_learning_rate")
+  _rename_opt(params, "maximum_iterations", "maximum_decoding_length")
+
+  clip_gradients = _delete_opt(params, "clip_gradients")
+  if clip_gradients:
+    optimizer_params = params.setdefault("optimizer_params", {})
+    optimizer_params["clipnorm"] = clip_gradients
+
+  weight_decay = _delete_opt(params, "weight_decay")
+  if weight_decay:
+    optimizer_params = params.setdefault("optimizer_params", {})
+    optimizer_params["weight_decay"] = weight_decay
+
+# Only covering the most common optimizers.
+_V1_OPTIMIZER_MAP = {
+    "AdamOptimizer": "Adam",
+    "GradientDescentOptimizer": "SGD",
+    "LazyAdamOptimizer": "LazyAdam",
+}
+
+def _convert_to_v2_optimizer(params):
+  optimizer = params.get("optimizer")
+  if optimizer:
+    v2_optimizer = _V1_OPTIMIZER_MAP.get(optimizer)
+    if not v2_optimizer:
+      raise ValueError("params/optimizer should be manually converted: no registered "
+                       "conversion for optimizer %s" % optimizer)
+    params["optimizer"] = v2_optimizer
+
+  optimizer_params = params.get("optimizer_params")
+  if optimizer_params:
+    _rename_opt(optimizer_params, "beta1", "beta_1")
+    _rename_opt(optimizer_params, "beta2", "beta_2")
+
+def _convert_to_v2_lr_schedules(params):
+  decay_type = params.get("decay_type")
+  if not decay_type:
+    return
+  if decay_type.startswith("noam_decay"):
+    params["decay_type"] = "NoamDecay"
+    if "decay_params" not in params:
+      model_dim = _delete_opt(params, "decay_rate")
+      warmup_steps = _delete_opt(params, "decay_steps")
+      params["decay_params"] = dict(model_dim=model_dim, warmup_steps=warmup_steps)
+  else:
+    raise ValueError("params/decay_type should be manually converted: no registered "
+                     "conversion for decay type %s" % decay_type)
+
+def _convert_to_v2_step_accumulation(params, config):
+  # Try to upgrade step accumulation to train/effective_batch_size.
+  decay_step_duration = _delete_opt(params, "decay_step_duration") or 1
+  gradients_accum = _delete_opt(params, "gradients_accum") or 1
+  accum_steps = max(decay_step_duration, gradients_accum)
+  if accum_steps > 1:
+    train_config = config.setdefault("train", {})
+    batch_size = train_config.get("batch_size")
+    if batch_size is not None and batch_size > 0:
+      train_config["effective_batch_size"] = batch_size * accum_steps
+    else:
+      raise ValueError("params/decay_step_duration and params/gradients_accum "
+                       "should be manually converted to train/effective_batch_size")
+
+def _delete_opt(config, name):
+  return config.pop(name, None)
+
+def _rename_opt(config, name, new_name):
+  value = _delete_opt(config, name)
+  if value is not None:
+    config[new_name] = value
+  return value
