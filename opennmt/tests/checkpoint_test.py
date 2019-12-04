Fix retrieval of variable from object-based name in checkpoint averaging (#564)
The previous logic incorrectly assumes how TensorFlow generates
variables name in the checkpoint. We now invoke a TensorFlow API to
deduce the mapping between variables and their name in the checkpoint.

opennmt/tests/checkpoint_test.py
@@ -5,11 +5,18 @@ import tensorflow as tf
 from opennmt.utils import checkpoint as checkpoint_util
 
 
+class _CustomDense(tf.keras.layers.Dense):
+
+  def add_weight(self, name, *args, **kwargs):
+    # This is to test the case where the variable name is different than the attribute name.
+    name += "_1"
+    return super(_CustomDense, self).add_weight(name, *args, **kwargs)
+
 class _DummyModel(tf.keras.layers.Layer):
 
   def __init__(self):
     super(_DummyModel, self).__init__()
-    self.layers = [tf.keras.layers.Dense(20), tf.keras.layers.Dense(20)]
+    self.layers = [tf.keras.layers.Dense(20), _CustomDense(20)]
 
   def call(self, x):
     for layer in self.layers: