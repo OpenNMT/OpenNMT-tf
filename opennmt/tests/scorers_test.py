2173fb2d21701f28d8356b615fdbd7334ba85d61
Guillaume Klein <guillaume.klein@systrangroup.com>
2020-05-28 16:18:39 +0200

    Improve robustness of PRFScorer

--- opennmt/tests/scorers_test.py
+++ opennmt/tests/scorers_test.py
@@ -2,35 +2,48 @@ import os
 
 import tensorflow as tf
 
+from opennmt.tests import test_util
 from opennmt.utils import scorers
 
 
 class ScorersTest(tf.test.TestCase):
 
-  def _make_perfect_hypothesis_file(self):
-    ref_path = os.path.join(self.get_temp_dir(), "ref.txt")
-    hyp_path = os.path.join(self.get_temp_dir(), "hyp.txt")
-    with open(ref_path, "w") as ref_file, open(hyp_path, "w") as hyp_file:
-      text = "Hello world !\nHow is it going ?\n"
-      ref_file.write(text)
-      hyp_file.write(text)
-    return ref_path, hyp_path
+  def _run_scorer(self, scorer, refs, hyps):
+    ref_path = test_util.make_data_file(os.path.join(self.get_temp_dir(), "ref.txt"), refs)
+    hyp_path = test_util.make_data_file(os.path.join(self.get_temp_dir(), "hyp.txt"), hyps)
+    return scorer(ref_path, hyp_path)
 
   def testBLEUScorer(self):
-    bleu_scorer = scorers.BLEUScorer()
-    ref_path, hyp_path = self._make_perfect_hypothesis_file()
-    score = bleu_scorer(ref_path, hyp_path)
+    refs = ["Hello world !", "How is it going ?"]
+    scorer = scorers.BLEUScorer()
+    score = self._run_scorer(scorer, refs, refs)
     self.assertEqual(100, int(score))
 
   def testROUGEScorer(self):
-    rouge_scorer = scorers.ROUGEScorer()
-    ref_path, hyp_path = self._make_perfect_hypothesis_file()
-    score = rouge_scorer(ref_path, hyp_path)
+    refs = ["Hello world !", "How is it going ?"]
+    scorer = scorers.ROUGEScorer()
+    score = self._run_scorer(scorer, refs, refs)
     self.assertIsInstance(score, dict)
     self.assertIn("rouge-l", score)
     self.assertIn("rouge-1", score)
     self.assertIn("rouge-2", score)
     self.assertAlmostEqual(1.0, score["rouge-1"])
+
+  def testPRFScorer(self):
+    scorer = scorers.PRFScorer()
+    score = self._run_scorer(scorer, refs=["TAG O TAG O O TAG TAG"], hyps=["TAG O O O TAG TAG O"])
+    expected_precision = 2 / 3
+    expected_recall = 2 / 4
+    expected_fscore = (
+        2 * (expected_precision * expected_recall) / (expected_precision + expected_recall))
+    self.assertAlmostEqual(score["precision"], expected_precision, places=6)
+    self.assertAlmostEqual(score["recall"], expected_recall, places=6)
+    self.assertAlmostEqual(score["fmeasure"], expected_fscore, places=6)
+
+  def testPRFScorerEmptyLine(self):
+    scorer = scorers.PRFScorer()
+    self._run_scorer(scorer, [""], ["O TAG"])
+    self._run_scorer(scorer, ["O TAG"], [""])
 
   def testMakeScorers(self):
 
