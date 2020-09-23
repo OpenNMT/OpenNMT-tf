628658733075aecba51b24a62719f6873e16eeb5
Guillaume Klein <guillaume.klein@systrangroup.com>
2020-09-23 10:18:56 +0200

    Update PyPI classifiers

--- setup.py
+++ setup.py
@@ -29,9 +29,11 @@ setup(
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Scientific/Engineering :: Artificial Intelligence"
     ],
     project_urls={
