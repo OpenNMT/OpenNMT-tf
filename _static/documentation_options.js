2518db64f4e51fe8745e092eeb35066ce69c374a
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-10-20 12:09:49 +0000

    Update docs after building Travis build 1891 of OpenNMT/OpenNMT-tf
    The docs were built from the tag 'v2.13.0' against the commit
    ff2f5a9c13b155195aa94ac288c8505d23445d70.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/OpenNMT/OpenNMT-tf/jobs/737385842.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy --build-tags --branch-whitelist --built-docs docs/build .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.12.1',
+    VERSION: '2.13.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
