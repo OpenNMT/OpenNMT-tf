c7870c2eaf18d3c7ac3de027e4b91f7bed795b77
Guillaume Klein <guillaume.klein@systrangroup.com>
2021-04-23 16:32:32 +0200

    Allow non fully defined gradient shape when accumulating gradients

--- opennmt/optimizers/utils.py
+++ opennmt/optimizers/utils.py
@@ -136,6 +136,4 @@ class GradientAccumulator(object):
             return
         self._accum_steps.assign(0)
         for gradient in self._gradients:
-            gradient.assign(
-                tf.zeros(gradient.shape, dtype=gradient.dtype), read_value=False
-            )
+            gradient.assign(tf.zeros_like(gradient), read_value=False)
