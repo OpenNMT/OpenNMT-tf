d8378314a28a0a9a3d1b786e6ecbc75d76f33f26
Guillaume Klein <guillaume.klein@systrangroup.com>
2021-07-06 18:00:06 +0200

    Cleanup examples of custom model definition

--- config/models/multi_features_transformer.py
+++ config/models/multi_features_transformer.py
@@ -17,26 +17,29 @@ data:
   target_vocabulary: target_vocab.txt
 """
 
-import tensorflow as tf
-import opennmt as onmt
+import opennmt
 
 
-def model():
-    return onmt.models.Transformer(
-        source_inputter=onmt.inputters.ParallelInputter(
-            [
-                onmt.inputters.WordEmbedder(embedding_size=512),
-                onmt.inputters.WordEmbedder(embedding_size=16),
-                onmt.inputters.WordEmbedder(embedding_size=64),
-            ],
-            reducer=onmt.layers.ConcatReducer(),
-        ),
-        target_inputter=onmt.inputters.WordEmbedder(embedding_size=512),
-        num_layers=6,
-        num_units=512,
-        num_heads=8,
-        ffn_inner_dim=2048,
-        dropout=0.1,
-        attention_dropout=0.1,
-        ffn_dropout=0.1,
-    )
+class MultiFeaturesTransformer(opennmt.models.Transformer):
+    def __init__(self):
+        super().__init__(
+            source_inputter=opennmt.inputters.ParallelInputter(
+                [
+                    opennmt.inputters.WordEmbedder(embedding_size=512),
+                    opennmt.inputters.WordEmbedder(embedding_size=16),
+                    opennmt.inputters.WordEmbedder(embedding_size=64),
+                ],
+                reducer=opennmt.layers.ConcatReducer(),
+            ),
+            target_inputter=opennmt.inputters.WordEmbedder(embedding_size=512),
+            num_layers=6,
+            num_units=512,
+            num_heads=8,
+            ffn_inner_dim=2048,
+            dropout=0.1,
+            attention_dropout=0.1,
+            ffn_dropout=0.1,
+        )
+
+
+model = MultiFeaturesTransformer
