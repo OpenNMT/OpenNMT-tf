16cc45cac1a9cf9dc411b505c2aa8fe7c5890b34
Guillaume Klein <guillaumekln@users.noreply.github.com>
2021-08-30 12:17:41 +0200

    Include SacreBLEU version 2.0 in requirements (#872)

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
         "pyonmttok>=1.26.4,<2;platform_system=='Linux' or platform_system=='Darwin'",
         "pyyaml>=5.3,<5.5",
         "rouge>=1.0,<2",
-        "sacrebleu>=1.5.0,<1.6",
+        "sacrebleu>=1.5.0,<2.1",
         "tensorflow-addons>=0.13,<0.14",
     ],
     extras_require={
