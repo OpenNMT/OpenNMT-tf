73f5993b47ebc8523a75dd9fa0d5986bb70d008d
Guillaume Klein <guillaume.klein@systrangroup.com>
2022-02-07 12:10:17 +0100

    TFLite tests are still failing with TensorFlow 2.8

--- opennmt/tests/tflite_test.py
+++ opennmt/tests/tflite_test.py
@@ -124,7 +124,7 @@ class TFLiteTest(tf.test.TestCase):
     @pytest.mark.skipif(
         version.parse("2.7.0")
         <= version.parse(tf.__version__)
-        < version.parse("2.8.0"),
+        < version.parse("2.9.0"),
         reason="Test case failing with TensorFlow 2.7",
     )
     def testTFLiteInterpreter(self, model, params=None, quantization=None):
