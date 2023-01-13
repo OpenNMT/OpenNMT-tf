18f630298b529ebc489f6c8d718b71d6a1a84a3e
guillaumekln <guillaumekln@users.noreply.github.com>
2023-01-13 15:50:38 +0000

    Deploying to gh-pages from  @ 6b6e68ad94a8e87c33222203bd6061f9f7492d1d 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.30.0',
+    VERSION: '2.31.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
